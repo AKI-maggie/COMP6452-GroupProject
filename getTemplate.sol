b17f0914f6738cc2a4ee2f27385321e14d133752
littlerest1 <891982668yzq@gmail.com>
2020-07-10 00:11:31 +1000

    Get receipt status function

--- getTemplate.sol
+++ getTemplate.sol
@@ -1,30 +1,41 @@
 pragma solidity ^0.4.22;
 import "github.com/provable-things/ethereum-api/provableAPI_0.4.25.sol";
+import "github.com/Arachnid/solidity-stringutils/strings.sol";
 
 contract ExampleContract is usingProvable {
-
-   string public ETHUSD;
+   using strings for *;
+   string public Receipt_Status;
+   string public link;
+   string public s3;
    event LogConstructorInitiated(string nextStep);
    event LogPriceUpdated(string price);
    event LogNewProvableQuery(string description);
 
    function ExampleContract() payable {
-       LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Provable Query.");
+       LogConstructorInitiated("Constructor was initiated. Call 'getStatus()' to send the Provable Query.");
    }
 
    function __callback(bytes32 myid, string result) {
        if (msg.sender != provable_cbAddress()) revert();
-       ETHUSD = result;
+       Receipt_Status = result;
        LogPriceUpdated(result);
    }
+   
+   function append(string a, string b, string c, string d, string e) internal pure returns (string) {
 
-   function updatePrice() payable {
+        return string(abi.encodePacked(a, b, c, d, e));
+
+   }
+
+   function getStatus(string restaurant, string receipt_N) payable {
        if (provable_getPrice("URL") > this.balance) {
            LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
        } else {
            LogNewProvableQuery("Provable query was sent, standing by for the answer..");
+           link = "json(https://guarded-sands-73970.herokuapp.com/records/" ;
+           s3 = append(link,restaurant,"/",receipt_N,  ").results.status");
            //provable_query("URL", "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).time");
-           provable_query("URL", "json(https://guarded-sands-73970.herokuapp.com/records).result.0.name");
+           provable_query("URL", s3);
        }
    }
 }
