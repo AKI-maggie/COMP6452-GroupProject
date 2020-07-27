c71ea6261bdc7eabdac778b2524f65c68128b683
Maggie_AKI <maggie.zhu2016@outlook.com>
2020-07-27 16:45:40 +1000

    Update Backend.sol

--- Backend.sol
+++ Backend.sol
@@ -1,11 +1,8 @@
 pragma solidity ^0.4.24;
-pragma experimental ABIEncoderV2;
 
 import "github.com/provable-things/ethereum-api/provableAPI_0.4.25.sol";
 import "github.com/Arachnid/solidity-stringutils/strings.sol";
 import "./StringTools.sol";
-
-import "./UserITF.sol";
 
 // This file contains the interface class which contains management functions for normal users
 contract Backend is usingProvable{
@@ -55,12 +52,17 @@ contract Backend is usingProvable{
     event LogDebug2(string description);
     mapping (string => Review) reviews;
     
+    string public debug;
+    
     // Receipt Authentication
     function Backend(address _m){
         st = StringTools(_m);
     }
-    function receiptAuthenticate(string memory receiptNo, string memory restName) payable public
-        notEmpty(receiptNo) notEmpty(restName) enoughPrice {
+    
+    function () payable {}
+    
+    function receiptAuthenticate(string memory receiptNo, string memory restName, address sender) payable public
+        enoughPrice notEmpty(receiptNo) notEmpty(restName)  {
         require(usedReceipts[st.append2(restName, receiptNo)] == address(0));
         string memory s3 = st.append(receiptLink,restName,"/",receiptNo,  ").result");
         if (requestPrice > this.balance) {
@@ -69,7 +71,7 @@ contract Backend is usingProvable{
         else {
           LogNewProvableQuery("Provable query was sent, standing by for the answer..");
           bytes32 queryId = provable_query(60,"URL", s3,500000);
-          validIds[queryId] = Usage('receipt', restName, msg.sender, '', 0);
+          validIds[queryId] = Usage('receipt', restName, sender, '', 0);
         }
     }
     
@@ -78,59 +80,61 @@ contract Backend is usingProvable{
     function __callback(bytes32 myid, string result) {
         if (msg.sender != provable_cbAddress()) revert();
         // interactions
+        debug=validIds[myid].use;
         if (st.compareStrings(validIds[myid].use, 'receipt')){
             // receipt Authentication
+            debug = result;
             if (st.compareStrings(result, 'false') != true){
                 string memory receipt_addr = st.append2(validIds[myid].arg1, result);
                 usedReceipts[receipt_addr] = validIds[myid].arg2;
-                LogDebug2("new receipt verified");
+                
             }
         }
-        else if (st.compareStrings(validIds[myid].use, 'review')){
+        // else if (st.compareStrings(validIds[myid].use, 'review')){
             
-            reviews[st.append2(validIds[myid].arg1, validIds[myid].arg3)] = Review(result, validIds[myid].arg2, validIds[myid].arg1, validIds[myid].arg3, validIds[myid].arg4);
-            //uploadipfs('review', data, msg.sender, restName, receiptNo, reviewCredit);
-            usedReceipts[st.append2(validIds[myid].arg1, validIds[myid].arg3)] = address(-1);  // the receipt no longer usable
+        //     reviews[st.append2(validIds[myid].arg1, validIds[myid].arg3)] = Review(result, validIds[myid].arg2, validIds[myid].arg1, validIds[myid].arg3, validIds[myid].arg4);
+        //     //uploadipfs('review', data, msg.sender, restName, receiptNo, reviewCredit);
+        //     usedReceipts[st.append2(validIds[myid].arg1, validIds[myid].arg3)] = address(-1);  // the receipt no longer usable
 
-        }
-        else if (st.compareStrings(validIds[myid].use, 'comment')){
+        // }
+        // else if (st.compareStrings(validIds[myid].use, 'comment')){
             
-            usedReceipts[st.append2(validIds[myid].arg1, validIds[myid].arg3)] = address(-1);  // the receipt no longer usable
-        }
-        else if (st.compareStrings(validIds[myid].use, 'prize')){
-            // users[validIds[myid].arg2].unusedCredits -= validIds[myid].arg4;
-            // users[validIds[myid].arg2].usedCredits += validIds[myid].arg4;
-            // users[validIds[myid].arg2].holdCoupons.push(result);
-        }
-        else if (st.compareStrings(validIds[myid].use, 'update')){
+        //     usedReceipts[st.append2(validIds[myid].arg1, validIds[myid].arg3)] = address(-1);  // the receipt no longer usable
+        // }
+        // else if (st.compareStrings(validIds[myid].use, 'prize')){
+        //     // users[validIds[myid].arg2].unusedCredits -= validIds[myid].arg4;
+        //     // users[validIds[myid].arg2].usedCredits += validIds[myid].arg4;
+        //     // users[validIds[myid].arg2].holdCoupons.push(result);
+        // }
+        // else if (st.compareStrings(validIds[myid].use, 'update')){
             
-        }
+        // }
         else{}
         delete validIds[myid];
     }
     
-    // IPFS upload function
-    // hash file link : https://guarded-sands-73970.herokuapp.com/records/ipfs 
-    // when you want to access the ipfs database link is : https://ipfs.infura.io/ipfs/ + {hash from above address}
-    function uploadipfs(string option, string data, address user, string restName, string receiptNo, int credit) payable enoughPrice{
-        bytes32 queryId;
-        if (provable_getPrice("URL") > this.balance) {
-           LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
-        } 
-        else {
-           LogNewProvableQuery("Provable query was sent, standing by for the answer..");
-           if(st.compareStrings(option,"comment")){
-               queryId = provable_query("URL","json(https://guarded-sands-73970.herokuapp.com/records/Comment).success.deposit",data);
-               validIds[queryId] = Usage('comment', restName, user, receiptNo, credit);
-           }
-           else if (st.compareStrings(option, "update")){
-            //   queryId = provable_query("URL","json(https://guarded-sands-73970.herokuapp.com/records/Comment).success.deposit",data);
-            //   validIds[queryId] = Usage('comment', restName, user, receiptNo, credit);
-           }
-           else{
-               queryId = provable_query("URL", "json(https://guarded-sands-73970.herokuapp.com/records/Review).success.deposit",data);
-               validIds[queryId] = Usage('review', restName, user, receiptNo, credit);
-           }
-       }
-    }
+    // // IPFS upload function
+    // // hash file link : https://guarded-sands-73970.herokuapp.com/records/ipfs 
+    // // when you want to access the ipfs database link is : https://ipfs.infura.io/ipfs/ + {hash from above address}
+    // function uploadipfs(string option, string data, address user, string restName, string receiptNo, int credit) payable enoughPrice{
+    //     bytes32 queryId;
+    //     if (provable_getPrice("URL") > this.balance) {
+    //       LogNewProvableQuery("Provable query was NOT sent, please add some ETH to cover for the query fee");
+    //     } 
+    //     else {
+    //       LogNewProvableQuery("Provable query was sent, standing by for the answer..");
+    //       if(st.compareStrings(option,"comment")){
+    //           queryId = provable_query("URL","json(https://guarded-sands-73970.herokuapp.com/records/Comment).success.deposit",data);
+    //           validIds[queryId] = Usage('comment', restName, user, receiptNo, credit);
+    //       }
+    //       else if (st.compareStrings(option, "update")){
+    //         //   queryId = provable_query("URL","json(https://guarded-sands-73970.herokuapp.com/records/Comment).success.deposit",data);
+    //         //   validIds[queryId] = Usage('comment', restName, user, receiptNo, credit);
+    //       }
+    //       else{
+    //           queryId = provable_query("URL", "json(https://guarded-sands-73970.herokuapp.com/records/Review).success.deposit",data);
+    //           validIds[queryId] = Usage('review', restName, user, receiptNo, credit);
+    //       }
+    //   }
+    // }
 }
